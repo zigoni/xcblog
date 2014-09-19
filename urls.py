30682f56575bf0417e6ee098784812a18a98ae44
Milanno Zigoni <zigoni@foxmail.com>
2014-09-20 00:41:01 +0800

    增加了日志显示页面，评论系统直接用了多说，也不知道效果咋样

--- urls.py
+++ urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('xcblog.views',
     url(r'^$', 'homepage', name='xc-homepage'),
+    url(r'^b(?P<pk>\d+)/$', 'show_blogpost', name='xc-show-blogpost'),
 )
--- views.py
+++ views.py
@@ -8,3 +8,19 @@ def homepage(request):
         'blogposts': blogposts,
     }
     return render(request, 'xcblog/homepage.html', context)
+
+def show_blogpost(request, pk):
+    blogpost = BlogPost.objects.get(pk=pk)
+    if blogpost.is_active == False:
+        context = {
+            'title': '博主暂时不打算公开这篇日志',
+        }
+        return render(request, 'xcblog/not_active.html', context)
+    else:
+        blogpost.read_times += 1
+        blogpost.save()
+        context = {
+            'title': blogpost.title,
+            'blogpost': blogpost,
+        }
+        return render(request, 'xcblog/show_blogpost.html', context)
