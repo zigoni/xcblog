cbece14efae3c0afe4513c6f6757bdf438c89130
Milanno Zigoni <zigoni@foxmail.com>
2014-09-21 01:13:19 +0800

    单独的日志分类页面以及路径导航条

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('xcblog.views',
     url(r'^$', 'homepage', name='xc-homepage'),
     url(r'^b(?P<pk>\d+)/$', 'show_blogpost', name='xc-show-blogpost'),
+    url(r'^c(?P<pk>\d+)/$', 'show_category', name='xc-show-category'),
 )
--- views.py
+++ views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from xcblog.models import BlogPost
+from django.shortcuts import get_object_or_404, render
+from xcblog.models import BlogCategory, BlogPost
 
 def homepage(request):
     blogposts = BlogPost.objects.filter(is_active=True)
@@ -10,7 +10,7 @@ def homepage(request):
     return render(request, 'xcblog/homepage.html', context)
 
 def show_blogpost(request, pk):
-    blogpost = BlogPost.objects.get(pk=pk)
+    blogpost = get_object_or_404(BlogPost, pk=pk)
     if blogpost.is_active == False:
         context = {
             'title': '博主暂时不打算公开这篇日志',
@@ -24,3 +24,13 @@ def show_blogpost(request, pk):
             'b': blogpost,
         }
         return render(request, 'xcblog/show_blogpost.html', context)
+
+def show_category(request, pk):
+    category = get_object_or_404(BlogCategory, pk=pk)
+    blogposts = BlogPost.objects.filter(is_active=True, category=category)
+    context = {
+        'title': category.name,
+        'c': category,
+        'blogposts': blogposts,
+    }
+    return render(request, 'xcblog/show_category.html', context)
